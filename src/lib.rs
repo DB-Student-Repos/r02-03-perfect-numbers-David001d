de7e41d8aab8d6af458babccad8bdce53a5aa755
David O <148862921+David001d@users.noreply.github.com>
2024-04-03 02:05:35 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,4 @@
-#[derive(Debug, PartialEq, Eq)]
+ #[derive(Debug, PartialEq, Eq)]
 pub enum Classification {
     Abundant,
     Perfect,
@@ -6,5 +6,15 @@ pub enum Classification {
 }
 
 pub fn classify(num: u64) -> Option<Classification> {
-    unimplemented!("classify {num}");
+    if num == 0 {
+        return None;
+    }
+    
+    let aliquot_sum: u64 = (1..num).filter(|&x| num % x == 0).sum();
+    
+    match aliquot_sum {
+        sum if sum == num => Some(Classification::Perfect),
+        sum if sum > num => Some(Classification::Abundant),
+        _ => Some(Classification::Deficient),
+    }
 }
